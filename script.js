e7dc0c1cb5a4880ef7208d765330012c242db32d
hellenribas <tchwtrabalho@gmail.com>
2021-12-09 22:17:37 -0300

    requisito 9 ok

--- script.js
+++ script.js
@@ -38,7 +38,26 @@ elemPixel.addEventListener('click',colocandoCor);
 function colocandoCor (event2) {
     let selected = document.querySelector('.selected');
     let cor = selected.style.backgroundColor;
-    elementTarget2 = event2.target;
-    elementTarget2.style.backgroundColor = cor;
+    console.log(cor);
+    event2.target.style.backgroundColor = cor;
     
+}
+let localBotao = document.getElementById('botao');
+function criarBotao (botao) {
+    botao = document.createElement('button');
+    let text = document.createTextNode('Limpar');
+    botao.appendChild(text);
+    botao.id = 'clear-board';
+    localBotao.appendChild(botao);
+}
+criarBotao();
+let botaoLimpar = document.getElementById('clear-board')
+botaoLimpar.addEventListener('click', tirandoCor)
+
+function tirandoCor() {
+    let pixelBoard2 = document.getElementsByClassName('pixel');
+    console.log(pixelBoard2);
+    for (index = 0; index < pixelBoard2.length; index += 1) {
+        pixelBoard2[index].style.backgroundColor = 'white';
+    }
 }
